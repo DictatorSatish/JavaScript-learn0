80500b7791e107c8fe9320f1f742178a37b07ae5
DictatorSatish <148948364+DictatorSatish@users.noreply.github.com>
2023-10-31 03:09:25 +0000

    Datatype

---
+++ Basic/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newre version of js by engine.
+
+//alert( 3+3)    // this not support in node js it's supportable only in browser.
+
+console.log("sat") // right way
+//console.log("sat") console.log("sat") // generate errors
+console.log("sat"); console.log("sat") // run successfully but not good programming
+console.log("sat"); // run successfully but not good programming
+
+
+//Datatype
+//number 
+//string
+//null
+//undefinde
+//symbol
+//object
+console.log(typeof "satish")
+console.log(typeof 456)
+console.log(typeof false)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof Symbol)
+
+//    using this command run the code "node Basic/datatypes.js"
---
+++ Basic/variables.js
@@ -0,0 +1,11 @@
+const accountId = 1234
+let accountEmail = " sj452920@gmail.com"
+var accountPassword = "Satish@123"
+accountCity = " Bhopal"
+// accountId = 45
+accountEmail = "sdij@gmail.com"
+accountPassword = " sjsd"
+accountCity = " rewa"
+let satish 
+console.log(accountId)
+console.table([accountEmail , accountPassword, accountId , accountCity,satish])
